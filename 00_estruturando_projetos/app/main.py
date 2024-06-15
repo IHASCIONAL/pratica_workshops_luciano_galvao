14dff12ed9523fc777f259b0a513c5b4e7ce0bd2
Ismael <ismaelhenriqueeu@gmail.com>
2024-06-15 19:35:19 -0300

    Implementando etapa de Extract

--- 00_estruturando_projetos/app/main.py
+++ 00_estruturando_projetos/app/main.py
@@ -1,1 +1,4 @@
-import pandas as pd
+from pipeline.extract import extract_from_excel
+
+lista_dataframes = extract_from_excel("../data/input")
+print(lista_dataframes)
---
+++ 00_estruturando_projetos/app/pipeline/extract.py
@@ -0,0 +1,30 @@
+import os
+import glob
+
+import pandas as pd
+
+from typing import List
+
+"""
+Função para ler os arquivos de uma pasta data/input e retornar uma lista de dataframes
+
+args: input_path(str): caminho da pasta com os arquivos
+
+return: lista de dataframes
+
+"""
+
+
+def extract_from_excel(path: str) -> List[pd.DataFrame]:
+    all_files = glob.glob(os.path.join(path, "*.xlsx"))
+
+    dataframe_list = []
+
+    for file in all_files:
+        dataframe_list.append(pd.read_excel(file))
+
+    return dataframe_list
+
+if __name__ == "__main__":
+    dataframe_list = extract_from_excel(path)
+    print(dataframe_list)
